bea9327610dac641d0552e7668bce61aa21d6d85
jeredublo <je.redublo@gmail.com>
2022-05-11 10:20:48 -0700

    Small changes

--- app/src/main/java/com/example/shotlist/w_project/list/ProjectAdapter.kt
+++ app/src/main/java/com/example/shotlist/w_project/list/ProjectAdapter.kt
@@ -9,8 +9,7 @@ import com.example.shotlist.w_project.data_structs.Project
 
 class ProjectAdapter(
     private val onClick: ((Project) -> Unit)? = null,
-    private val onLongClick: ((Project) -> Unit)? = null,
-) : ListAdapter<Project, ProjectViewHolder>(DiffCallback())
+    private val onLongClick: ((Project) -> Unit)? = null, ) : ListAdapter<Project, ProjectViewHolder>(DiffCallback())
 {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
